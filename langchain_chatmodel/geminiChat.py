dc42a88a71d0913d2fbeaceae82adfbfe773e47e
atom017 <khaingjar@gmail.com>
2024-10-02 13:23:33 +0630

    first commit

---
+++ langchain_chatmodel/geminiChat.py
@@ -0,0 +1,21 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage, SystemMessage
+
+# Setup environment variables and messages
+load_dotenv()
+
+messages = [
+    SystemMessage(content="Solve the following math problems"),
+    HumanMessage(content="What is 81 divided by 9?"),
+]
+
+
+# ---- Google Chat Model Example ----
+
+# https://console.cloud.google.com/gen-app-builder/engines
+# https://ai.google.dev/gemini-api/docs/models/gemini
+model = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+
+result = model.invoke(messages)
+print(f"Answer from Google: {result.content}")
